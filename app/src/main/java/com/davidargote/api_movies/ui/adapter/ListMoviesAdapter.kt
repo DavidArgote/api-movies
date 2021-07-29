f270f08421da17238b50e1b66aec51c0e185cb1c
David Argote <argote.2075@gmail.com>
2021-07-29 17:03:13 -0500

    [ADD] Adapter

---
+++ app/src/main/java/com/davidargote/api_movies/ui/adapter/ListMoviesAdapter.kt
@@ -0,0 +1,8 @@
+package com.davidargote.api_movies.ui.adapter
+
+import androidx.recyclerview.widget.RecyclerView
+import com.davidargote.api_movies.model.remote.Movie
+
+class ListMoviesAdapter constructor(private val moviesList: List<Movie>) {
+
+}

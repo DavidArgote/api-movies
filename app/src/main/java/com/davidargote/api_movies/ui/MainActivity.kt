bd60694ba34a06c426ee41a74f0acbdc1151a8a0
David Argote <argote.2075@gmail.com>
2021-07-29 11:32:59 -0500

    First commit

---
+++ app/src/main/java/com/davidargote/api_movies/ui/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.davidargote.api_movies.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.davidargote.api_movies.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
